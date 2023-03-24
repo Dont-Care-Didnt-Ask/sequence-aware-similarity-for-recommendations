14e7d1b0c85c2a07b65c3713d52cfcc036becba7
Mikhail Seleznyov <simplymike8@gmail.com>
2023-03-24 06:16:56 +0300

    Add scaling to SeqSVD

--- seq_svd.py
+++ seq_svd.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ilupp
 
+import scipy
 from scipy.sparse.linalg import svds
 from scipy.sparse import csr_matrix, lil_matrix, csc_matrix
 from scipy.sparse import eye as speye
@@ -32,32 +33,21 @@ def build_seq_svd_model(config, training, data_description):
     weight_description = data_description.copy()
     weight_description["feedback"] = "weights"
     time_interactions = generate_interactions_matrix(training_with_time_ranks, weight_description)
-    #print("Computing jaccard similarity...")
 
     if config["similarity_type"] == "jaccard":
         S = jaccard_similarity(time_interactions)
     elif config["similarity_type"] == "cosine":
         S = csr_matrix(cosine_similarity(time_interactions, time_interactions))
-    #print("Similarity matrix shape:", S.shape)
-    #print("S density:", S.getnnz() / np.prod(S.shape))
 
     L = ilupp.icholt(S, add_fill_in=0, threshold=config["incomplete_cholesky_threshold"])
 
     alpha = config["sequence_similarity_coef"]
     L = alpha * L + (1 - alpha) * speye(S.shape[0])
-    #print("L matrix shape:", L.shape)
-    #print("L density:", L.getnnz() / np.prod(L.shape))
 
     matrix = generate_interactions_matrix(training, data_description).astype(np.float32)
+    weights = np.power(matrix.getnnz(axis=0) + 1, 0.5 * (config["scaling"] - 1))
     
-    #print("Interaction matrix shape:", matrix.shape)
-    #print("Matrix density:", matrix.getnnz() / np.prod(matrix.shape))
-
-    matrix = L.T.dot(matrix)
-
-    #print("L * Interaction matrix shape:", matrix.shape)
-    #print("L * Matrix density:", matrix.getnnz() / np.prod(matrix.shape))
-
+    matrix = L.T.dot(matrix.dot(scipy.sparse.diags(weights)))
 
     _, _, vt = svds(matrix, k=config["rank"], return_singular_vectors="vh")
     item_factors = np.ascontiguousarray(vt[::-1, :].T)
@@ -72,46 +62,23 @@ def svd_scoring(params, data, data_description):
     return scores
 
 
-def seq_svd_gridsearch(ranks, sequence_similarity_coefs, training, testset, holdout, data_description, topn):
-    max_rank = max(ranks)
-    config = {"rank": max_rank}
-    results = []
-
-    for sequence_similarity_coef in sequence_similarity_coefs:
-        print(sequence_similarity_coef)
-        config["sequence_similarity_coef"] = sequence_similarity_coef
-        item_factors = build_jaccard_svd_model(config, training, data_description)
-
-        for rank in ranks:
-            item_factors_trunc = item_factors[:, :rank]
-            scores = svd_scoring(item_factors_trunc, testset, data_description)
-            recs = topn_recommendations(scores, topn)
-            metric = model_evaluate(recs, holdout, data_description, topn)
-
-            results.append({
-                "rank": rank, "metric": metric, "sequence_similarity_coef": sequence_similarity_coef,
-            })
-        
-    return results
-
-
-
-def full_seq_svd_gridsearch(ranks, sequence_similarity_coefs, powers, thresholds, similarity_types,
+def full_seq_svd_gridsearch(ranks, sequence_similarity_coefs, powers, thresholds, similarity_types, scalings,
         training, testset, holdout, data_description, topn):
     max_rank = max(ranks)
     config = {"rank": max_rank}
     results = []
 
-    grid = product(sequence_similarity_coefs, powers, thresholds, similarity_types)
+    grid = product(sequence_similarity_coefs, powers, thresholds, similarity_types, scalings)
 
-    for sequence_similarity_coef, power, threshold, sim_type in grid:
+    for sequence_similarity_coef, power, threshold, sim_type, scaling in grid:
         print(f"Sequence similarity coef: {sequence_similarity_coef}, power: {power}, "
-              f"threshold: {threshold}, similarity type: {sim_type}")
+              f"threshold: {threshold}, similarity type: {sim_type}, scaling: {scaling}")
         
         config["incomplete_cholesky_threshold"] = threshold
         config["sequence_similarity_coef"] = sequence_similarity_coef
         config["similarity_type"] = sim_type
         config["power"] = power
+        config["scaling"] = scaling
 
         item_factors = build_seq_svd_model(config, training, data_description)
 
@@ -127,6 +94,7 @@ def full_seq_svd_gridsearch(ranks, sequence_similarity_coefs, powers, thresholds
                 "sequence_similarity_coef": sequence_similarity_coef,
                 "similarity_type": sim_type,
                 "power": power,
+                "scaling": scaling,
                 "metric": metric,
             })
         
